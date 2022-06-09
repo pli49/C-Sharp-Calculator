dca24b215e97c08b32da7ba4b652425f2f6caec5
Bill Liu <pli49@uclive.ac.nz>
2022-06-09 15:16:38 +1200

    developed two testcases for add

---
+++ Calculator/Calculator.Unit.Test/CalculatorEngineTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Unit.Test
+{
+
+
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbleOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate(number1, "add", number2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForSymbleOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate(number1, "+", number2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
